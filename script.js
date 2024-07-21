6bd692daa9f6f982d2d00112361696e2ccb52bd0
Faisal <faisalirfan7c@gmail.com>
2024-07-21 23:08:33 +0530

    Added Game Logic

--- script.js
+++ script.js
@@ -1,62 +1,107 @@
+let humanScore = 0;
+let computerScore = 0;
+let selection = document.getElementById('player_img')
+let botSelection = document.getElementById('bot_img')
+let gameStart = document.getElementsByClassName('ctrl_btn')
+let result = document.getElementById('round-results');
+
 function getComputerChoice(){
     let arr = ['rock','paper','scissors'];
     let index = Math.floor(Math.random() * 3);
     return arr[index];
 }
 
-function getHumanChoice(){
-    let input = prompt("Enter Rock, Paper or Scissors").toLowerCase();
-    if(input=="rock" || input=="paper" || input=="scissors"){
-        return input;
-    }
-    return 0;
-}
+function startGame(){
+    const rock = document.getElementById('rock_btn');
+    rock.addEventListener('click',()=>{
+        selection.src = "Images/rock.png";
+        //console.log("rock");
+        updateScore('rock',comp_play());
+    });
 
-let humanScore = 0;
-let computerScore = 0;
+    const paper = document.getElementById('paper_btn');
+    paper.addEventListener('click',()=>{
+        selection.src = "Images/paper.png";
+        //console.log("paper");
+        updateScore('paper',comp_play());
+    });
 
-function playGame(){
-    
-    for(let i=0;i<5;i++){
-        let humanChoice = getHumanChoice();
+    const scissors = document.getElementById('scissors_btn');
+    scissors.addEventListener('click',()=>{
+        selection.src = "Images/scissors.png";
+        //console.log("scissors");
+        updateScore('scissors',comp_play());
+    });
+
+    function comp_play(){
         let computerChoice = getComputerChoice();
-        /*console.log(humanChoice);
-        console.log(computerChoice);*/
-        playRound(humanChoice,computerChoice);
-    }  
+        console.log(computerChoice);
+        
+        if(computerChoice=="rock"){
+            botSelection.src = "Images/rock.png";
+        }
+        else if(computerChoice=="paper"){
+            botSelection.src = "Images/paper.png";
+        }
+        else{
+            botSelection.src = "Images/scissors.png";
+        }
+        return computerChoice;
+    };
 }
 
-function playRound(humanChoice,computerChoice){
-    humanSelection = humanChoice.toLowerCase();
-    computerSelection = computerChoice.toLowerCase();
+function updateScore(humanChoice,computerChoice){
+    humanSelection = humanChoice;
+    computerSelection = computerChoice;
     if(humanSelection==computerSelection){
+        result.textContent = `Tie! ${humanSelection} and ${computerSelection} are equal`;
         console.log(`Tie! ${humanSelection} and ${computerSelection} are equal`);
     }
     else if((humanSelection=="paper" && computerSelection=="rock") || (humanSelection=="rock" && computerSelection=="scissors") || (humanSelection=="scissors" && computerSelection=="paper")){
+        result.textContent = `You Win! ${humanSelection} beats ${computerSelection}`;
         console.log(`You Win! ${humanSelection} beats ${computerSelection}`);
         humanScore++;
+        document.getElementById('human_score').textContent = `You: ${humanScore}`;
     }
     else{
+        result.textContent = `You Lose! ${computerSelection} beats ${humanSelection}`;
         console.log(`You Lose! ${computerSelection} beats ${humanSelection}`);
         computerScore++;
+        document.getElementById('computer_score').textContent = `Computer: ${computerScore}`;
     }
 }
 
-playGame();
-
-console.log(`Player Score: ${humanScore}`);
-console.log(`Computer Score: ${computerScore}`);
-if(humanScore>computerScore){
-    console.log("Player Wins!");
-}
-else if(humanScore==computerScore){
-    console.log("Tie!");
-}
-else{
-    console.log("Computer Wins!");
-}
-    
+startGame();
 
 
 
 
+
+
+//function playGame(){
+//    
+//    for(let i=0;i<5;i++){
+//        let humanChoice = getHumanChoice();
+//        let computerChoice = getComputerChoice();
+//        /*console.log(humanChoice);
+//        console.log(computerChoice);*/
+//        playRound(humanChoice,computerChoice);
+//    }  
+//}
+//
+
+
+//playGame();
+//
+//console.log(`Player Score: ${humanScore}`);
+//console.log(`Computer Score: ${computerScore}`);
+//if(humanScore>computerScore){
+//    console.log("Player Wins!");
+//}
+//else if(humanScore==computerScore){
+//    console.log("Tie!");
+//}
+//else{
+//    console.log("Computer Wins!");
+//}
+//
